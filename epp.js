e6727bfd82392e95b6c23e027323775553b1d23b
kings <530675800@qq.com>
2015-07-03 15:09:16 +0800

    add readme

---
+++ epp.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var express = require('express');
+var url = require('url');
+var path = require('path');
+var os = require('os');
+
+// 服务器网络信息
+var ifaces = os.networkInterfaces();
+var addresses = [];
+
+for (var i in ifaces) {
+	for (var ii in ifaces[i]) {
+		var address = ifaces[i][ii];
+		if (address.family === 'IPv4') {
+			addresses.push(address.address);
+		}
+	}
+
+};
+
+var app = express();
+
+app.set('views', __dirname + '/public');
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// router
+app.get('/', function(req, res) {
+	res.render('demo');
+})
+
+
+app.listen(8000, function() {
+	console.log('Welcome to Dev');
+	console.log('Server runing at localhost:8000 - ');
+	console.log('    __  __  _  _    __      __\n    ||\\//|  \\\\//   /  \\\\   (/_\n    || v |   ||    \\__//   __/)')
+	console.log('===============================');
+	console.log('本地请访问: localhost:8000 \n         或 '+ addresses[1]+':8000');
+	console.log('内网请访问: '+ addresses[0]+':8000');
+
+
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+### iServer
+iojs server 
+用于前端开发,可以使用 ejs 来创建文件模板,从而减轻生成重复页面时,添加相同的内容时的重复功作.
+包含 GenneratePages 功能
+
+查看 [GenneratePages](https://github.com/ektx/Node/tree/master/GenneratePages)
