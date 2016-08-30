49216ebe5bace66fc90560afb0232389c16c9c0d
ektx <530675800@qq.com>
2016-08-30 17:37:09 +0800

    添加表单验证

--- server/js/login.js
+++ server/js/login.js
@@ -7,6 +7,8 @@ $(function() {
 
 	// 登录 注册 忘记密码切换功能
 	$('.login-help a').click(function(e) {
+		e.preventDefault()
+
 		var _ = $(this);
 		var _p = _.parents('.login-mod');
 		var _class = _.attr('class');
@@ -28,46 +30,134 @@ $(function() {
 		var user = $('.login-usr-name input').val();
 		var pwd = $('.login-usr-pwd input').val();
 
-		var _ = $(this);
-		var data = {};
-
-		_.find('input').each(function(e) {
-			var _this = $(this);
-
-			data.[_this.attr('name')] = _this.val();
+		$(this).myVerification({
+			errBox: '.error-info',
+			show: 'show'
 		})
 
-		console.log(data)
-
-		// if (user && pwd) {
-		// 	$.ajax({
-		// 		url: '/loginIn',
-		// 		type: 'POST',
-		// 		dataType: 'json',
-		// 		data: {
-		// 			user: user,
-		// 			passwd: pwd
-		// 		}
-		// 	})
-		// 	.done(function(data) {
-		// 		if (!data.success) {
-		// 			$('.error-info').addClass('show').text(data.msg)
-		// 		} else {
-		// 			if (location.hash && location.hash.length > 9) {
-		// 				location.href = location.hash.substr(9)
-		// 			} else {
-		// 				location.href = data.msg
-		// 			}
-		// 		}
-		// 	})
-		// 	.fail(function(err){
-		// 		$('.error-info').show().text(err)
-		// 	})
-			
-		// } else {
-		// 	$('.error-info').addClass('show').text( '用户名或密码不能为空' )
-		// }
-
+	}).keyup(function(e) {
+		$(this).myVerification({
+			errBox: '.error-info',
+			show: 'show'
+		})
 	})
 
 });
+
+$.fn.hasAttr = function(name) {
+	var attr = this.attr(name)
+	return typeof attr !== typeof undefined && attr !== false
+}
+
+$.fn.myVerification = function(options) {
+
+	var _ = this;
+	var data = {};
+	var url = _.attr('action');
+	var type = _.attr('method');
+
+	console.log(event)
+
+	var setError = function(it, ele, info) {
+		if (options.show) {
+			_.prev(ele).addClass(options.show).html(info)
+		} else {
+			_.prev(ele).html(info)
+		}
+
+		if (event.type === 'submit') {
+			it.focus()
+		}
+	}
+
+	var removeErr = function(ele) {
+		if (options.show) _.prev(ele).removeClass(options.show);
+		else _.prev(ele).html('')
+	}
+
+	_.find('input').each(function(e) {
+
+		var _this = $(this);
+		var _name = _this.attr('name');
+
+		if (event.type === 'keyup' && event.target.name !== _name) {
+			return true
+		}
+
+		var _val = $.trim(_this.val());
+		var _placeholder = _this.hasAttr('placeholder') ? _this.attr('placeholder') : '';
+
+
+		// 非空验证
+		if (_this.hasAttr('required')) {
+
+			// 当指定了显示错误的容器时,此时逐个验证信息
+			// if (_this.attr('type') === 'text' ) {
+				if ( _val === '' ) {
+					setError(_this, options.errBox, _placeholder + '不能为空')
+					// 当指定了显示错误的容器时,此时逐个验证信息
+					if (options.errBox) return false;
+				} else {
+					removeErr(_this)
+				}
+
+			// }
+
+			// 邮箱验证
+			if (_this.attr('type') === 'email' ) {
+				if ( !/\w+@\w+\.\w+/.test(_val) ) {
+					setError(_this, options.errBox, _placeholder + '格式不正确')
+					// 当指定了显示错误的容器时,此时逐个验证信息
+					if (options.errBox) return false;
+				} else {
+					removeErr(_this)
+				}
+
+			}
+
+			// 密码验证
+			if (_this.attr('type') === 'password' ) {
+				if ( /\s/g.test(_val) ) {
+					setError(_this, options.errBox, _placeholder + '不能出现空格')
+					// 当指定了显示错误的容器时,此时逐个验证信息
+					if (options.errBox) return false;
+				} else {
+					removeErr(_this)
+				}
+
+			}
+		}
+
+		console.log(_this.hasAttr('required'))
+		console.log()
+
+		data[_this.attr('name')] = _val;
+
+	})
+
+	$.ajax({
+		url: url,
+		type: type,
+		dataType: 'json',
+		data: data
+	})
+	.done(function(data) {
+		if (!data.success) {
+			$('.error-info').addClass('show').text(data.msg)
+		} else {
+			if (location.hash && location.hash.length > 9) {
+				location.href = location.hash.substr(9)
+			} else {
+				location.href = data.msg
+			}
+		}
+	})
+	.fail(function(err){
+		$('.error-info').show().text(err)
+	})
+	
+
+	console.log(url)
+	console.log(type)
+	console.log(data)
+}
