0333b6a9a30489c337e4eae6a9bb0b59b4a696c7
ektx <530675800@qq.com>
2019-04-11 10:56:15 +0800

    修改启动入口

--- index.js
+++ index.js
@@ -3,27 +3,30 @@
 const program = require('commander')
 const main = require('./bin/main')
 const osInfo  = require('./package')
+
 const version = osInfo.version
 
 program
 	.version(version)
-	.option('-b, --browser [name]', '开启服务器同时打开指定浏览器[chrome|firefox|ie|opera]')
-	.option('-p, --port [port]', '自定义端口号')
-	.option('-s, --http', '启动 HTTP 协议,默认启用 HTTPS')
+	.option('-b, --browser [name]', 'turn on the server and open the specified browser, name as [chrome|firefox|ie|opera]')
+	.option('-p, --port [port]', 'set server port, default 8080')
+    .option('-s, --https', 'use HTTPS ,default HTTP')
 
 program.on('--help', () => {
-	console.log('  例如:  启动服务器并打开默认浏览器: iserver -p 9000 -b\n')
+	console.log(`
+  Quick Strat: iserver
+
+  Use Option:
+    
+    eg: Start the server and open the default browser: 
+    iserver -p 9000 -b
+`)
 })
 
 program.parse(process.argv)
 
-// 默认端口为 8000
-program.port = typeof program.port === 'string' ? parseInt(program.port) : 8000
+// 默认端口为 8080
+program.port = typeof program.port === 'string' ? parseInt(program.port) : 8080
 
 // 启动
-main({
-	browser: Boolean(program.browser),
-	port: program.port,
-	version,
-	https: !program.http
-})
+main({...program, version})
