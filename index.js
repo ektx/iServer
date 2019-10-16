59750bf05be98f3c6e3774e6284955e2efb80177
wuming <530675800@qq.com>
2019-10-16 11:50:36 +0800

    📝完善文档与细节调整

--- index.js
+++ index.js
@@ -28,7 +28,11 @@ program.parse(process.argv)
 
 // 默认端口为 8080
 program.port = typeof program.port === 'string' ? parseInt(program.port) : 8080
+
 // 默认监听用户浏览器访问目录
+// 0 不使用文件监听功能，文件的变化，系统不提示
+// 1 只监听访问过的文件目录，当访问过的文件发生变化会通告客户端
+// 2 启动服务时同时开启监听所有根目录中的文件，文件变化后通知客户端，与1相比，好处是一次监听所有内容变化;不利在于启动时占CPU需求一些时间
 program.watch = typeof program.watch === 'string' ? parseInt(program.watch) : 1
 
 // 启动
