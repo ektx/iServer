919085288eebd49c2696da452cb766476f84528f
wuming <530675800@qq.com>
2019-10-16 11:50:36 +0800

    📝完善文档与细节调整

--- bin/watch.js
+++ bin/watch.js
@@ -1,12 +1,11 @@
 const chokidar = require('chokidar')
-const path = require('path')
 
 // 标注监控完成
 let isReady = false
-let watchFiles = []
 let socket = null
 let watcher = null
 let timer = null
+let initLevel = undefined
 
 function watchDir (addr, depth) {
   watcher = chokidar.watch(addr, {
@@ -14,7 +13,6 @@ function watchDir (addr, depth) {
       if (
         ['.DS_Store', '.git', 'node_modules'].includes(filePath)
       ) {
-        // console.log(filePath)
         return true
       }
     },
@@ -24,7 +22,7 @@ function watchDir (addr, depth) {
   // 监听变化
   watcher.on('all', (evt, path) => {
     if (timer) clearTimeout(timer)
-    // console.log(isReady)
+    // 对首次加入监听的文件，不输出提示
     if (isReady) {
       // 输出变化
       console.log(evt.toUpperCase(), path)
@@ -35,25 +33,34 @@ function watchDir (addr, depth) {
       })
     }
 
+    // 如果文件加入监听后，对变化进行响应
     timer = setTimeout(() => {
       isReady = true
     }, 100)
   })
 
   watcher.on('ready', () => {
-    console.log('Ready')
+    console.log('Web Socket is Ready')
     isReady = true
     // console.log(watcher.getWatched())
   })
 }
  
+/**
+ * 默认事件
+ * @param {Socket} io 
+ * @param {number} level 控制监听文件的层级
+ */
 exports.watchInit = function (io, level) {
   socket = io
+  initLevel = level
   watchDir(process.cwd(), level)
 }
 
 exports.watchAdd = function (address) {
-  console.log('Address')
-  isReady = false
-  watcher.add(address)
+  // 如果用户开启的是监听访问目录时
+  if (initLevel === 0) {
+    isReady = false
+    watcher.add(address)
+  }
 }
