f1226ba622bac4c7df30f75a5c0ca9798a389d3c
wenlong <530675800@qq.com>
2020-09-24 23:48:04 +0800

    chore:细节调整

--- src/route.ts
+++ src/route.ts
@@ -1,12 +1,13 @@
 // import fs from 'fs'
 // import path from 'path'
 import Router from '@koa/router'
+import { Context } from 'koa'
 // import koaBody from 'koa-body'
 
 const router = new Router()
 
 router
-  .get('/', async (ctx: any) => {
+  .get('/', async (ctx: Context): Promise<void> => {
     ctx.body = 'hello ts'
   })
 
