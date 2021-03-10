fdc425b6423b1a25540c74ab51c8deae64c48026
wenlong <530675800@qq.com>
2021-03-10 14:26:11 +0800

    perf:优化ip获取为异步方案

--- src/ip.ts
+++ src/ip.ts
@@ -5,20 +5,22 @@ import macaddress from 'macaddress'
  * 获取当前服务器的 IP
  * @returns 返回 IPv4 与 IPv6
  */
-async function getIPS() {
-  const mac = await macaddress.one()
-  const ip = {}
-  const ips = networkInterfaces()
-
-  for (let key in ips) {
-    ips[key].forEach(item => {
-      if (item.mac === mac) {
-        (<any>ip)[item.family] = item.address
-      }
-    })
-  }
-
-  return ip
+function getIPs(): Promise<{IPv4?: string, IPv6?: string}> {
+  return new Promise(async (resolve) => {
+    const mac = await macaddress.one()
+    const ip = {}
+    const ips = networkInterfaces()
+  
+    for (let key in ips) {
+      ips[key].forEach(item => {
+        if (item.mac === mac) {
+          (<any>ip)[item.family] = item.address
+        }
+      })
+    }
+  
+    resolve(ip)
+  })
 }
 
 async function isServer(ip: string) {
@@ -28,14 +30,15 @@ async function isServer(ip: string) {
     result = true
   } else {
     let ipv4 = (<any>ip).match(/\d.+/)[0]
+    let ips = await getIPs()
 
-    result = (<any>(await getIPS())).IPv4 === ipv4
+    result = ips.IPv4 === ipv4
   }
 
   return result
 }
 
 export {
-  getIPS,
+  getIPs,
   isServer
 }
